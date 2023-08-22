1ae75b383b49243cd6273a4a969fbc047e3dbf13
Ingkonneh <konnehidrissa31@gmail.com>
2023-08-21 19:48:44 -0700

    C - Pointers, arrays and strings

--- 0x05-Pointers_arrays_strings/main.h
+++ 0x05-Pointers_arrays_strings/main.h
@@ -15,10 +15,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-#ifndef MAIN_H
-#define MAIN_H
-
 int _atoi(char *s);
 
-#endif /* MAIN_H */
 #endif
